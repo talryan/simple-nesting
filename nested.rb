d5d51745b67bd97f49b618183ce4e8b3ed1001ba
talryan <nataliamryan@gmail.com>
2020-10-27 09:03:26 -0700

    commit tree 98bc54c96e6ca2a51eb8e4a25dc4a824f8b89457

--- nested.rb
+++ nested.rb
@@ -1,7 +1,7 @@
 
 def hopper
-	programmer_hash = 
- 		{
+  programmer_hash =
+   {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -15,6 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
+    
+     programmer_hash[:grace_hopper]
 
 
 end
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,7 +84,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:yukihiro_matsumoto] = {}
+     programmer_hash
     
 end
 
@@ -101,6 +107,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+  
+	programmer_hash[:alan_kay][:known_for] = alans_new_info
+	programmer_hash
      
      
 end
@@ -122,4 +131,6 @@ def adding_to_dennis
         }
      }
 
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+     programmer_hash
 end
